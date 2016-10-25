69755772d13e7e1a71aea8c6625f9843a1c7f617
Kimihiro N <kimihiro.nisimura@gmail.com>
2016-10-25 09:55:58 +0900

    WIP: DataBinding

--- app/src/main/kotlin/com/appspot/pistatium/dotstream/viewmodels/ContentViewModel.kt
+++ app/src/main/kotlin/com/appspot/pistatium/dotstream/viewmodels/ContentViewModel.kt
@@ -1,1 +1,19 @@
 package com.appspot.pistatium.dotstream.viewmodels
+
+import android.content.Context
+
+
+interface ContentViewModelInterface {
+    interface MainView {
+        fun getContext(): Context
+        fun loadData()
+    }
+
+    interface ViewModel {
+        fun destroy()
+    }
+}
+
+class ContentViewModel(val mainView: ContentViewModelInterface.MainView, val context: Context) : ContentViewModelInterface {
+
+}
