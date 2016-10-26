bf58a49596bdf703cbb08f7997238389b801c570
Kimihiro N <kimihiro.nisimura@gmail.com>
2016-10-26 09:57:06 +0900

    WIP: DataBind

--- app/src/main/kotlin/com/appspot/pistatium/dotstream/MainActivity.kt
+++ app/src/main/kotlin/com/appspot/pistatium/dotstream/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.appspot.pistatium.dotstream
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.appspot.pistatium.dotstream.databinding.ActivityMainBinding
+import com.appspot.pistatium.dotstream.viewmodels.ContentViewModel
+import com.appspot.pistatium.dotstream.viewmodels.ContentViewModelInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ContentViewModelInterface.MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val content = ContentViewModel(this, applicationContext)
+        binding.contentViewModel = content
     }
 }
