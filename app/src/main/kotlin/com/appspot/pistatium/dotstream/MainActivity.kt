fe9eaad9e67743ee73fa6043141ab2926c3e7297
kimihiro_n <kimihiro.nisimura@gmail.com>
2016-10-29 21:30:58 +0900

    WIP: ViewModel Binding

--- app/src/main/kotlin/com/appspot/pistatium/dotstream/MainActivity.kt
+++ app/src/main/kotlin/com/appspot/pistatium/dotstream/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.appspot.pistatium.dotstream
 
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.appspot.pistatium.dotstream.databinding.ActivityMainBinding
 import com.appspot.pistatium.dotstream.viewmodels.ContentViewModel
+import com.appspot.pistatium.dotstream.viewmodels.ContentViewModelInterface
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), ContentViewModelInterface.MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val content = ContentViewModel()
+        val content = ContentViewModel(this, applicationContext)
+        binding.contentViewModel = content
+    }
+
+    override fun loadData() {
+
+    }
+
+    override fun getContext(): Context {
+        return this
     }
 }
--- app/src/main/kotlin/com/appspot/pistatium/dotstream/viewmodels/ContentViewModel.kt
+++ app/src/main/kotlin/com/appspot/pistatium/dotstream/viewmodels/ContentViewModel.kt
@@ -1,19 +1,23 @@
 package com.appspot.pistatium.dotstream.viewmodels
 
 import android.content.Context
+import com.appspot.pistatium.dotstream.BuildConfig
 
-//
-//interface ContentViewModelInterface {
-//    interface MainView {
-//        fun getContext(): Context
-//        fun loadData()
-//    }
-//
-//    interface ViewModel {
-//        fun destroy()
-//    }
-//}
 
-class ContentViewModel() {
-    val test = "test"
+interface ContentViewModelInterface {
+    interface MainView {
+        fun getContext(): Context
+        fun loadData()
+    }
+
+    interface ViewModel {
+        fun destroy()
+    }
+}
+
+class ContentViewModel(val mainView: ContentViewModelInterface.MainView, val context: Context) {
+    var test: String = ""
+    init {
+        test = BuildConfig.TEST
+    }
 }
